dbfcb8ec6fc8d702ee06223f11485367237ef2ab
Virginia Garcia <ve.garcia.ortiz@gmail.com>
2019-07-14 14:11:27 -0400

    Updates

---
+++ precache-manifest.a56a8d4780ffac38cd6bcb44ef61b9f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e7f0c995cfed5d028413f722c891b7e",
+    "url": "/viscosho/index.html"
+  },
+  {
+    "revision": "7b9c1af78c9bb3506ab0",
+    "url": "/viscosho/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/viscosho/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "7b9c1af78c9bb3506ab0",
+    "url": "/viscosho/static/js/main.fb291aef.chunk.js"
+  },
+  {
+    "revision": "06529008a4f65dd278a9",
+    "url": "/viscosho/static/js/runtime~main.f61471a4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/viscosho/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.95badc45055358bcf460f18909b43e12.js"
+  "/viscosho/precache-manifest.a56a8d4780ffac38cd6bcb44ef61b9f1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/viscosho/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
