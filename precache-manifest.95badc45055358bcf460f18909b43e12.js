b61232e7e2f8a7db8156d364f8068128e2d16087
Virginia Garcia <ve.garcia.ortiz@gmail.com>
2019-07-14 13:56:21 -0400

    Updates

---
+++ precache-manifest.95badc45055358bcf460f18909b43e12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8660237717acc041d7a1f86bc8447e3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59a1fbfc23ea47cbc9af",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "59a1fbfc23ea47cbc9af",
+    "url": "/static/js/main.81bf7dc6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
